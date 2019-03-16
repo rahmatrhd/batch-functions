7de2acaa678792befe559b9cb8e107df63a24bba
Rahmat Hidayat <kickyou44@gmail.com>
2019-03-16 20:43:33 +0700

    first function: store user data in firestore

---
+++ functions/src/admin.ts
@@ -0,0 +1,10 @@
+import * as admin from "firebase-admin";
+
+const adminConfig = JSON.parse(process.env.FIREBASE_CONFIG as string);
+
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+  databaseURL: adminConfig.databaseURL,
+});
+
+export default admin;
---
+++ functions/src/users/storeAuthenticatedUser.ts
@@ -0,0 +1,13 @@
+import * as functions from "firebase-functions";
+import admin from "../admin";
+
+export default functions.auth.user().onCreate((user) => {
+  admin.firestore().collection("users").doc(user.uid).set({
+    displayName: user.displayName,
+    email: user.email,
+    emailVerified: user.emailVerified,
+    phoneNumber: user.phoneNumber,
+    photoURL: user.photoURL,
+    uid: user.uid,
+  });
+});
